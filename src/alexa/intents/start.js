955ccea36eb5d7a2c710df40e5fb8a205937f64b
John McKim <johncmckim@gmail.com>
2016-11-29 10:25:02 -0800

    Limit the number of servers you can start and handle errors gracefully

--- src/alexa/intents/start.js
+++ src/alexa/intents/start.js
@@ -1,20 +1,42 @@
 'use strict';
 
+const BbPromise = require('bluebird');
 const chaosService = require('../../chaos-service');
 
+const getTextResult = (text) => ({
+    sessionAttributes: {},
+    cardTitle: "Start",
+    speechOutput: text,
+    repromptText: "",
+    shouldEndSession: true
+});
+
 module.exports = (intent) => {
+    const MAX_SERVERS = 20;
     const countSlot = intent.slots ? intent.slots.Count : null;
 
     const count = countSlot ? countSlot.value : 1;
+    const serversText = count === 1 ? 'server' : 'servers';
+
+    if(count > MAX_SERVERS) {
+        return BbPromise.resolve(getTextResult(`
+            Woah Pony. You tried to start ${count} ${serversText}.
+            But we only allow starting ${MAX_SERVERS} servers at a time.`
+        ));
+    }
 
     return chaosService
         .start({ count, size: 't2.nano' })
-        .then(() => ({
-            sessionAttributes: {},
-            cardTitle: "Kill",
-            speechOutput:
-                `I started ${count} ${count === 1 ? 'server' : 'servers'} for you.`,
-            repromptText: "",
-            shouldEndSession: true
-        }));
+        .then((result) => {
+            if(result.errorType) {
+                switch(result.errorType) {
+                    case 'InstanceLimitExceeded':
+                        return getTextResult(`You instance limit is exceeded. You cannot start ${count} more ${serversText}`);
+                    default:
+                        return getTextResult('There was a problem starting those servers');
+                }
+            }
+
+            return getTextResult(`I started ${count} ${serversText} for you.`);
+        });
 };

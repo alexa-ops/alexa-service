530936d88f461848b35b7619aeb2c6b67a0b4128
John McKim <johncmckim@gmail.com>
2016-11-30 22:39:23 -0800

    Update help text

--- src/alexa/intents/help.js
+++ src/alexa/intents/help.js
@@ -8,8 +8,8 @@ module.exports = () => (BbPromise.resolve({
     speechOutput:
         `Welcome to reinvent 2016!
         This skill has implemented Netflix's chaos monkey program to randomly kill a server in our AWS test account.
-        You can ask me to kill, crush or destroy.
+        You can ask me to start or kill instances, and also query how many are running.
         Why not give it a try?`,
-    repromptText: "You can ask me to kill, crush or destroy.",
+    repromptText: "You can ask me to start or kill instances, and also query how many are running",
     shouldEndSession: false
 }));
